38f0b9f0ebf406e16eec62c3f466005d41a766a3
Sven Gächter <entwicklung@allround-service.biz>
2019-11-16 22:58:07 +0100

    v1.0 First Release

--- mailcatcher.py
+++ mailcatcher.py
@@ -5,6 +5,8 @@
 #                                                                          #
 #   History:                                                               #
 #   0.1: Initial quick and dirty Code                                      #
+#   1.0: First Release                                                     #
+#                                                                          #
 ############################################################################
 
 from imap_tools import MailBox, Q
@@ -25,54 +27,51 @@ fileExt = config['General']['fileExt']
 
 ##### Log-Handler #####
 logging.basicConfig(filename=os.path.join(os.path.dirname(__file__),'Mailcatcher.log'),format='%(asctime)s - %(message)s', level=config['General']['loglevel'])
-logging.info('-------------------------')
-logging.info(f"{__file__} gestartet...")
 
 
-mailbox = MailBox(mailServer)
-mailbox.login(login, password, initial_folder='INBOX')
-"""
-try:
-    mailbox.folder.create('INBOX/folder1')
-except:
-    print('Ordner existiert schon')
-"""
 
-for msg in mailbox.fetch(Q(from_=senderAddr)):
-    Subject = msg.subject
-    timestamp = msg.date
-    logging.debug(f'Betreff: {Subject} von {timestamp}')
-    if len(msg.attachments) > 0:
-        attachment = [i[0] for i in msg.attachments] + [i[1] for i in msg.attachments]
+def processMailbox():
+    with MailBox(mailServer).login(login, password, initial_folder='INBOX') as mailbox:
+        logging.debug(sg.subject for msg in mailbox.fetch())
 
-        if fileExt in attachment[0]:
-            logging.info(f'{attachment[0]} wird heruntergeladen...')
-            f = open(os.path.join(attPath, attachment[0]), "wb") #destFilename If the Filename will not be replaced
-            f.write(attachment[1])
-            f.close()
-            try:
-                mailbox.move(msg.uid, 'INBOX/processed')
-                logging.debug(f'Mail verschoben.')
-                continue
-            except:
-                logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
-                continue
-        else:
-            logging.info(f'{attachment[0]} entspricht nicht dem Suchbegriff {fileExt}. Datei wurde nicht heruntergeladen')
-            try:
-                mailbox.move(msg.uid, 'INBOX/rejected')
-                logging.debug(f'Mail verschoben.')
-                continue
-            except:
-                logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
-                continue
-    else:
-        logging.debug(f'Mail hat keinen Anhang')
-        try:
-            mailbox.move(msg.uid, 'INBOX/rejected')
-        except:
-            logging.debug(f'Mail mit dem Betreff {Subject} konnte nicht verschoben werden')
-            continue
+        for msg in mailbox.fetch(Q(from_=senderAddr)):
+            Subject = msg.subject
+            timestamp = msg.date
+            logging.debug(f'Betreff: {Subject} von {timestamp}')
+            if len(msg.attachments) > 0:
+                attachment = [i[0] for i in msg.attachments] + [i[1] for i in msg.attachments]
 
-mailbox.logout()
-logging.info(f"{__file__} gestoppt.")
+                if fileExt in attachment[0]:
+                    logging.info(f'{attachment[0]} wird heruntergeladen...')
+                    f = open(os.path.join(attPath, attachment[0]), "wb") #destFilename If the Filename will not be replaced
+                    f.write(attachment[1])
+                    f.close()
+                    try:
+                        mailbox.move(msg.uid, 'INBOX/processed')
+                        logging.debug(f'Mail verschoben.')
+                        continue
+                    except:
+                        logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
+                        continue
+                else:
+                    logging.info(f'{attachment[0]} entspricht nicht dem Suchbegriff {fileExt}. Datei wurde nicht heruntergeladen')
+                    try:
+                        mailbox.move(msg.uid, 'INBOX/rejected')
+                        logging.debug(f'Mail verschoben.')
+                        continue
+                    except:
+                        logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
+                        continue
+            else:
+                logging.debug(f'Mail hat keinen Anhang')
+                try:
+                    mailbox.move(msg.uid, 'INBOX/rejected')
+                except:
+                    logging.debug(f'Mail mit dem Betreff {Subject} konnte nicht verschoben werden')
+                    continue
+
+if __name__ == "__main__":
+    logging.info('-------------------------')  
+    logging.info(f"{__file__} gestartet...")
+    processMailbox()
+    logging.info(f"{__file__} gestoppt.")
