ce4393daaba83d4b56102e6815591c933189f0e1
Sven Gaechter <sven.gaechter@tgsoft.ch>
2019-11-18 09:29:27 +0100

    bugfixign, Codestrapping inserted Possibility to define Dest Folder for Mail moving

--- mailcatcher.py
+++ mailcatcher.py
@@ -24,20 +24,17 @@ attPath = config['General']['attPath']
 mailServer = config['General']['mailServer']
 destFilename = config['General']['destFilename']
 fileExt = config['General']['fileExt']
+moveMail = config['General']['moveMailTo']
 
 ##### Log-Handler #####
 logging.basicConfig(filename=os.path.join(os.path.dirname(__file__),'Mailcatcher.log'),format='%(asctime)s - %(message)s', level=config['General']['loglevel'])
-
-
 
 def processMailbox():
     with MailBox(mailServer).login(login, password, initial_folder='INBOX') as mailbox:
         logging.debug(sg.subject for msg in mailbox.fetch())
 
         for msg in mailbox.fetch(Q(from_=senderAddr)):
-            Subject = msg.subject
-            timestamp = msg.date
-            logging.debug(f'Betreff: {Subject} von {timestamp}')
+            logging.debug(f'Betreff: {msg.subject} von {msg.date}')
             if len(msg.attachments) > 0:
                 attachment = [i[0] for i in msg.attachments] + [i[1] for i in msg.attachments]
 
@@ -47,11 +44,11 @@ def processMailbox():
                     f.write(attachment[1])
                     f.close()
                     try:
-                        mailbox.move(msg.uid, 'INBOX/processed')
+                        mailbox.move(msg.uid, moveMail)
+                        #mailbox.delete(msg.id)
                         logging.debug(f'Mail verschoben.')
-                        continue
                     except:
-                        logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
+                        logging.debug(f'Mail mit dem Betreff {msg.subject} konnte nicht verschoben werden')
                         continue
                 else:
                     logging.info(f'{attachment[0]} entspricht nicht dem Suchbegriff {fileExt}. Datei wurde nicht heruntergeladen')
@@ -60,18 +57,18 @@ def processMailbox():
                         logging.debug(f'Mail verschoben.')
                         continue
                     except:
-                        logging.debug(f'Mail mit dem Betreff {subject} konnte nicht verschoben werden')
+                        logging.debug(f'Mail mit dem Betreff {msg.subject} konnte nicht verschoben werden')
                         continue
             else:
                 logging.debug(f'Mail hat keinen Anhang')
                 try:
                     mailbox.move(msg.uid, 'INBOX/rejected')
                 except:
-                    logging.debug(f'Mail mit dem Betreff {Subject} konnte nicht verschoben werden')
+                    logging.debug(f'Mail mit dem Betreff {msg.subject} konnte nicht verschoben werden')
                     continue
 
 if __name__ == "__main__":
-    logging.info('-------------------------')  
+    logging.info('-------------------------')
     logging.info(f"{__file__} gestartet...")
     processMailbox()
-    logging.info(f"{__file__} gestoppt.")
+    logging.info(f"{__file__} beendet.")
